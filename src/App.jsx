95622b52c94a81edeb7bd1682c72191079e5e812
zehraasimsek <zhrasmsk1999@gmail.com>
2025-07-03 01:24:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <p className="text-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
